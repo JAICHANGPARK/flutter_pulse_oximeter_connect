0fe1e4c226bae86fda0dfcbf3d2fca3af9900360
JAICHANG.PARK <jaichang@angel-robotics.com>
2021-03-23 17:30:57 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(MyApp());
@@ -29,66 +33,128 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _incrementCounter() {}
+
+  checkSystemPermission() async {
+    var status = await Permission.location.status;
+    if (status.isDenied) {
+      await Permission.location.request();
+      // We didn't ask for permission yet or the permission has been denied before but not permanently.
+    }
+
+// You can can also directly ask the permission about its status.
+    if (await Permission.location.isRestricted) {
+      // The OS restricts access, for example because of parental controls.
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    checkSystemPermission();
+  }
+
+  BluetoothDevice? bluetoothDevice;
+  StreamSubscription? deviceStateStreamSubscription;
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    deviceStateStreamSubscription?.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title!),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Expanded(
+              child: StreamBuilder<List<ScanResult>>(
+                  stream: FlutterBlue.instance.scanResults,
+                  initialData: [],
+                  builder: (c, snapshot) {
+                    snapshot.data!.forEach((element) {
+                      print("${element.device.id} / ${element.device.name}");
+                      if (element.device.name == "J1") {
+                        print(">>> Oximeter Founded!!");
+                        bluetoothDevice = element.device;
+                        FlutterBlue.instance.stopScan();
+                      }
+                    });
+                    return ListTile();
+                  }),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              child: Column(
+                children: [
+                  Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  Text(
+                    '$_counter',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                  ElevatedButton(
+                      onPressed: bluetoothDevice == null
+                          ? null
+                          : () async {
+                              deviceStateStreamSubscription = bluetoothDevice?.state.listen((event) {
+                                if (BluetoothDeviceState.disconnected == event) {
+                                  print("BluetoothDeviceState.disconnected");
+                                } else if (BluetoothDeviceState.connected == event) {
+                                  print("BluetoothDeviceState.connected");
+                                }
+                              });
+                              await bluetoothDevice?.connect(autoConnect: false);
+                            },
+                      child: Text("Connected")),
+                  ElevatedButton(
+                      onPressed: bluetoothDevice == null
+                          ? null
+                          : () async {
+                              List<BluetoothService> services = await bluetoothDevice!.discoverServices();
+                              services.forEach((element) {
+                                print("service: ${element.uuid.toString()}");
+                                element.characteristics.forEach((element2) {
+                                  print("characteristics: ${element2.uuid.toString()}");
+                                });
+                              });
+                            },
+                      child: Text("Discovery")),
+                  ElevatedButton(
+                      onPressed: () async {
+                        setState(() {});
+                      },
+                      child: Text("refresh")),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: StreamBuilder<bool>(
+        stream: FlutterBlue.instance.isScanning,
+        initialData: false,
+        builder: (c, snapshot) {
+          if (snapshot.data!) {
+            return FloatingActionButton(
+              child: Icon(Icons.stop),
+              onPressed: () => FlutterBlue.instance.stopScan(),
+              backgroundColor: Colors.red,
+            );
+          } else {
+            return FloatingActionButton(
+                child: Icon(Icons.search), onPressed: () => FlutterBlue.instance.startScan(timeout: Duration(seconds: 4)));
+          }
+        },
+      ),
     );
   }
 }
