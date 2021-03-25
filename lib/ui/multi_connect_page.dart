924bea13b9adc4a5b882fb1f7d1b997bc2875dd2
JAICHANG.PARK <jaichang@angel-robotics.com>
2021-03-25 10:20:51 +0900

    Create multi_connect_page.dart

---
+++ lib/ui/multi_connect_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MultiConnectPage extends StatefulWidget {
+  MultiConnectPage({Key? key}) : super(key: key);
+
+  @override
+  _MultiConnectPageState createState() => _MultiConnectPageState();
+}
+
+class _MultiConnectPageState extends State<MultiConnectPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
