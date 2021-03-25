82b8a001ca13d95a76dac8414cf25e65ca08b5d6
JAICHANG.PARK <jaichang@angel-robotics.com>
2021-03-25 11:22:31 +0900

    Update multi_connect_page.dart

--- lib/ui/multi_connect_page.dart
+++ lib/ui/multi_connect_page.dart
@@ -3,6 +3,17 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 
+class J1Device{
+  BluetoothDevice? bluetoothDevice;
+  BluetoothCharacteristic? requestBluetoothCharacteristic0;
+  BluetoothCharacteristic? requestBluetoothCharacteristic1;
+  BluetoothCharacteristic? dataBluetoothCharacteristic0;
+  BluetoothCharacteristic? dataBluetoothCharacteristic1;
+
+  J1Device({this.bluetoothDevice, this.requestBluetoothCharacteristic0, this.requestBluetoothCharacteristic1,
+    this.dataBluetoothCharacteristic0, this.dataBluetoothCharacteristic1});
+}
+
 class MultiConnectPage extends StatefulWidget {
   MultiConnectPage({Key? key}) : super(key: key);
 
@@ -12,7 +23,7 @@ class MultiConnectPage extends StatefulWidget {
 
 class _MultiConnectPageState extends State<MultiConnectPage> {
   StreamSubscription? scanStreamSubscription;
-  Map<String, BluetoothDevice> bluetoothDevices = Map();
+  Map<String, J1Device> bluetoothDevices = Map();
   @override
   void initState() {
     // TODO: implement initState
@@ -44,7 +55,7 @@ class _MultiConnectPageState extends State<MultiConnectPage> {
                       print("${event.device.name} / ${event.device.id}");
                       if(event.device.name == "J1"){
                         print(">>> J1 Detected ");
-                        bluetoothDevices["${event.device.id}"] = event.device;
+                        bluetoothDevices["${event.device.id}"] = J1Device(bluetoothDevice: event.device);
                       }
                   });
                 }, child: Text("Start Scan")),
@@ -58,8 +69,11 @@ class _MultiConnectPageState extends State<MultiConnectPage> {
             ),
             ElevatedButton(onPressed: ()async{
               print(">>> bluetoothDevices.length : ${bluetoothDevices.length}");
-
             }, child: Text("Check Devices")),
+            ElevatedButton(onPressed: ()async{
+              print(">>> bluetoothDevices.length : ${bluetoothDevices.length}");
+            }, child: Text(" Connect Devices")),
+
           ],
         ),
       ),
