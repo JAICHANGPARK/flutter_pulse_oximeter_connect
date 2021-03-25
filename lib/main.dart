af27f7f951050bd362fe207b7e0f38b1bcedb0c5
JAICHANG.PARK <jaichang@angel-robotics.com>
2021-03-25 10:20:49 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,20 @@
-import 'dart:async';
-import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_blue/flutter_blue.dart';
-import 'package:flutter_pulse_oximeter_connect/enums/enum_detect.dart';
-import 'package:flutter_pulse_oximeter_connect/service/pulse_oximeter/j1/j1_ble_gatt_service.dart';
-import 'package:permission_handler/permission_handler.dart';
-
-import 'ui/oscilloscope.dart';
+import 'ui/single_connect_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pulse Oximeter Connect',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Pulse Oximeter Connect App'),
+      home: SingleConnectPage(title: 'Pulse Oximeter Connect App'),
     );
   }
 }
